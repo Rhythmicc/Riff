88567837b495695b47c60058c151ebc719c2d744
Rhythmicc <rhythmlian.cn@gmail.com>
2026-08-01 22:26:58 +0800

    Remove panel edge halo

---
+++ Tests/RiffTests/MaterialPanelTests.swift
@@ -0,0 +1,14 @@
+import AppKit
+import XCTest
+@testable import Riff
+
+final class MaterialPanelTests: XCTestCase {
+    @MainActor
+    func testTransparentMaterialPanelDoesNotAddSystemShadow() {
+        let controller = MaterialPanelController(size: NSSize(width: 320, height: 80))
+
+        XCTAssertFalse(controller.panel.isOpaque)
+        XCTAssertEqual(controller.panel.backgroundColor, .clear)
+        XCTAssertFalse(controller.panel.hasShadow)
+    }
+}
